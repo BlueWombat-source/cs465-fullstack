00be2b005e1719009ef9bfe51b309ea2491857ce
Bryce <bcampbell0101@outlook.com>
2026-08-15 19:27:45 -0400

    Module 6 completed baseline

---
+++ app_admin/src/app/edit-trip/edit-trip.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
+import { TripData } from '../services/trip-data';
+import { Trip } from '../models/trip';
+
+@Component({
+  selector: 'app-edit-trip',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './edit-trip.html',
+  styleUrl: './edit-trip.css',
+})
+export class EditTrip implements OnInit {
+  editForm!: FormGroup;
+  trip!: Trip;
+  submitted = false;
+  message: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private tripData: TripData
+  ) {}
+
+  ngOnInit(): void {
+
+  
+    // retrieve the tripCode from local storage
+    let tripCode = localStorage.getItem('tripCode');
+    if(!tripCode) {
+      alert("Something wrong, couldn't find where I stashed tripCode!");
+      this.router.navigate(['']);
+      return;
+    }
+
+    console.log('EditTripComponent::ngOnInit');
+    console.log('tripCode: ' + tripCode);
+
+    this.editForm = this.formBuilder.group({
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.tripData.getTrip(tripCode)
+      .subscribe({
+        next: (value: any) => {
+
+          const trip = value[0];
+
+          trip.start = new Date(trip.start).toISOString().substring(0, 10);
+
+          // populate our record into the form
+          this.editForm.patchValue(trip);
+
+          if(!value) {
+            this.message = 'No Trip Retrieved!';
+          }
+          else {
+            this.message = 'Trip: ' + tripCode + ' retrieved';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error ' + error);
+        }
+      })
+  }
+
+  public onSubmit() {
+    this.submitted = true;
+
+    if (this.editForm.valid) {
+      this.tripData.updateTrip(this.editForm.value)
+        .subscribe({
+          next: (value: any) => {
+          console.log(value);
+          this.router.navigate(['']);
+          },
+          error: (error: any) => {
+            console.log('Error: ' + error);
+          }
+        })
+    }
+  }
+
+  // get the form short name to access the form fields
+  get f() { return this.editForm.controls; }
+}
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,11 +6,15 @@ const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripsList
+    .get(tripsController.tripsList) // GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); // POST Method adds a Trip
+    
 
-// GET Method routes tripsFindByCode
+// GET Method routes tripsFindByCode - requires parameter tripCode
+// PUT Method routes tripsUpdateTrip - requires parameter tripCode
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
